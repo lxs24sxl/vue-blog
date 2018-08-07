d6b4a8de06b099fed506578b95891cdd700d6f08
lxs24sxl <13539748633@139.com>
2018-08-07 10:37:10 +0800

    完善关于我页面

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,7 +1,64 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <img src="http://eps.ink/img/bob-icon.png">
+  <div class="about-wrapper">
+    <h1>个人信息</h1>
+    <div class="about-message">
+      <pre>
+      {
+        basic: {
+          name: "Xiaoshun Lin",
+          code_name: "lxs24sxl",
+          age: "21",
+          gender: "male",
+          data_of_birth: "February 11, 1997",
+          occupation: "Front-end development",
+          country: "china",
+          province: "Guangdong",
+          city: "Guangzhou",
+          languages: {
+            Chinese: "native", 
+            English: "basic"
+          },
+          hobbies: [
+            "basketball",
+            "Web Designing"
+          ]
+        },
+        sklls: {
+          front-end_languages: [
+            "webpack", 
+            "vue.js", 
+            "mini programs, 
+            "react.js", 
+            "javascript", 
+            "html5", 
+            "underscore.js", 
+            "css/html", 
+            "bootstrap"
+          ],
+          computer_software: [
+            "VsCode", 
+            "Sublime Text", 
+            "Android Studio", 
+            "Microsoft office", 
+            "so on"
+          ],
+          certificate: [ 
+            "CET-4", 
+            "NCRE 3-Software Testing",
+            "China National Scholarship(2017)",
+            "National Endeavor Scholarship(2016)"
+          ],
+          education: [
+            {
+              time: "2015.09-2018-07",
+              name: "Guangdong AIB Polytechnic College"
+            },
+          ]
+        }
+      }
+    </pre>
+    </div>
+
   </div>
 </template>
 <script>
@@ -23,7 +80,19 @@ export default {
 };
 </script>
 <style lang="scss">
-.test {
-  height: '20px';
+.about {
+  &-wrapper {
+    max-width: 1110px;
+    text-align: center;
+    margin: 0 auto;
+    margin-top: 10px;
+    background-color: #fff;
+    h1 {
+      padding-top: 10px;
+    }
+  }
+  &-message {
+    text-align: left;
+  }
 }
 </style>
